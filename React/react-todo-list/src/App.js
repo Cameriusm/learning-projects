3b8d675146d170c71376fe9c43e69f1f1dde5c75
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2020-07-16 15:29:07 +0700

    React Todo List
    List of tasks created using the React library

---
+++ React/react-todo-list/src/App.js
@@ -0,0 +1,100 @@
+import React, { useEffect } from 'react';
+import TodoList from './Todo/TodoList';
+import Context from './context';
+// import AddTodo from './Todo/AddTodo';
+import Loader from './Loader';
+import Modal from './Modal/Modal';
+
+const AddTodo = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import('./Todo/AddTodo'));
+      }, 3000);
+    })
+);
+
+function App() {
+  const [todos, setTodos] = React.useState(
+    []
+    //   [
+    //   {
+    //     id: 1,
+    //     completed: false,
+    //     title: 'Купить хлеб',
+    //   },
+    //   {
+    //     id: 2,
+    //     completed: true,
+    //     title: 'Купить масло',
+    //   },
+    //   {
+    //     id: 3,
+    //     completed: false,
+    //     title: 'Купить молоко',
+    //   },
+    // ]
+  );
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React project</h1>
+        <Modal></Modal>
+
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+
+        {loading && <Loader />}
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
+      </div>
+    </Context.Provider>
+  );
+}
+
+export default App;
