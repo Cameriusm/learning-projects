a9be5d4a54b595ebcac78a100984912a02240bd2
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2020-09-18 21:28:59 +0700

    React-Complete-Course
    Completed React practice course

---
+++ React/React-Complete-Course/poketimes/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa nihil
+        fugiat, facere saepe est soluta deleniti repellat dignissimos nisi
+        mollitia qui ad corporis. Recusandae magni soluta minus sint et.
+        Consectetur?
+      </p>
+    </div>
+  );
+};
+
+export default Rainbow(About);
---
+++ React/React-Complete-Course/poketimes/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = (props) => {
+  //   setTimeout(() => {
+  //     props.history.push('./about');
+  //   }, 2000);
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa nihil
+        fugiat, facere saepe est soluta deleniti repellat dignissimos nisi
+        mollitia qui ad corporis. Recusandae magni soluta minus sint et.
+        Consectetur?
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
