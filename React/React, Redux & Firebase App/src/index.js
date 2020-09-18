8aaf23d5825385a1ead02f58d7887b3569ff5f4e
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2020-09-18 21:31:48 +0700

    React-Redux-Firebase-App
    Made React-Redux app using Firebase

---
+++ React/React, Redux & Firebase App/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {
+  createFirestoreInstance,
+  reduxFirestore,
+  getFirestore,
+} from 'redux-firestore';
+import firebase from 'firebase/app';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig)
+  )
+);
+const profileSpecificProps = {
+  userProfile: 'users',
+  useFirestoreForProfile: true,
+  enableRedirectHandling: false,
+  resetBeforeLogin: false,
+};
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  // eslint-disable-next-line
+  config: profileSpecificProps,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
