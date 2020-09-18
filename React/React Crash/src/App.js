b5f04bb798cf2edf20bd46cd44fa5c9ca168f558
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2020-09-18 21:30:10 +0700

    React-Crash
    Completed React Crash course

---
+++ React/React Crash/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HelloWorld from './Components/HelloWorld';
+// import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <div>
+      <Header />
+      <HelloWorld name="Jared" />
+      {/* <CounterExample /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
