f6b9bd75843e06bc27d4ebefd93a2f11045906ee
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2020-11-19 17:54:19 +0700

    Expense-Tracker
    Created initial react application for Expense Tracker

---
+++ React/expense-tracker-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+function App() {
+  return (
+    <div>
+     <Header />
+     <div className="container">
+       <Balance />
+     </div>
+    </div>
+  );
+}
+
+export default App;
