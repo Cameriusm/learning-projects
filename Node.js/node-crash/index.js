4f13bef2abc53eae5abb62c4cfe4cb1b0c64b98a
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2020-12-05 21:33:22 +0700

    Other examples + EventEmitter
    The rest of the modules and EventEmitter

--- Node.js/node-crash/index.js
+++ Node.js/node-crash/index.js
@@ -1,5 +1,13 @@
-const Person = require('./Person');
+// const Person = require('./Person');
 
-const person1 = new Person('John Doe', 30);
+// const person1 = new Person('John Doe', 30);
 
-person1.greeting();
+// person1.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello world!');
