703e47d1f2373b4f0fb4b7932db6684187761a2e
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2020-12-30 13:46:30 +0700

    Rendering Templates

--- Node.js/express-js/routes/api/member.js
+++ Node.js/express-js/routes/api/member.js
@@ -32,6 +32,40 @@ router.post('/', (req, res) => {
   }
   members.push(newMember);
   res.json(members);
+  // res.redirect('/');
+});
+
+// Update Member
+
+router.put('/:id', (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    const updMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+        res.json({ msg: 'Member updated', member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json({
+      msg: 'Member deleted',
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      ),
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 module.exports = router;
