ebf42e9c7b753bd0d3e47c83f4a2562ec3340ab5
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-01-04 18:33:01 +0700

    Exprense Tracker Backend

---
+++ React/expense-tracker-react/routes/transactions.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction,
+} = require('../controllers/transactions');
+
+// router.get('/', (req, res) => res.send('Hello'));
+router.route('/').get(getTransactions).post(addTransaction);
+
+router.route('/:id').delete(deleteTransaction);
+
+module.exports = router;
