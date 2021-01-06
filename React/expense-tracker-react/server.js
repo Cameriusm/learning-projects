cfda4ebf594ceb9ee55fb83d851b5ac587d83153
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-01-06 19:17:06 +0700

    Embed MongoDB in Expense-Tracker RestAPI

--- React/expense-tracker-react/server.js
+++ React/expense-tracker-react/server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
 const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/transactions', transactions);
 
