05d68933bfdae4cf030269f9344e1bfcb551c41a
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-01-14 23:41:53 +0700

    Add the ability for the server to send a simple response

--- Node.js/node-basics/server.js
+++ Node.js/node-basics/server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write('<p>hello, guys</p>');
+  res.write('<p>hello again, guys</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
