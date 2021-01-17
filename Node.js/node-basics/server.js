7b67cf2fc124ba19f7589553af3b731f86d3f06b
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-01-17 18:38:08 +0700

    Send html page from server to url on request

--- Node.js/node-basics/server.js
+++ Node.js/node-basics/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
@@ -6,10 +7,37 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
-  res.write('<head><link rel="stylesheet" href="#"></head>');
-  res.write('<p>hello, guys</p>');
-  res.write('<p>hello again, guys</p>');
-  res.end();
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  // send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, 'localhost', () => {
