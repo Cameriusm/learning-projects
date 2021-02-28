be48dc4c26947aaa698cbfb4d1be8475979797e5
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-02-28 18:30:11 +0700

    Add POST request for project create

--- Node.js/node-basics/Node Simple Server/app.js
+++ Node.js/node-basics/Node Simple Server/app.js
@@ -29,6 +29,8 @@ app.set('view engine', 'ejs');
 //middleware & static files
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(morgan('dev'));
 
 // // mongoose and mongo sandbox routes
@@ -118,6 +120,20 @@ app.get('/blogs', (req, res) => {
       console.log(err);
     });
 });
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
